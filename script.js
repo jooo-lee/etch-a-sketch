82eb2de31125cd22b473704579df51910ff62dec
Joseph Lee <josephlee3004@gmail.com>
2023-02-15 15:29:13 -0500

    Add header

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const canvas = document.getElementById("canvas");
 function makeGrid(size) {
     canvas.style.setProperty("--grid-rows", size);
     canvas.style.setProperty("--grid-cols", size);
+
+    // Maintain a grid size of 480px no matter how many squares there are in the grid
     canvas.style.setProperty("--grid-square-size", getGridSquareSize(size));
+
     for (let i = 0; i < (size * size); i++) {
         let gridSquare = document.createElement("div");
         gridSquare.classList.add("grid-square");
         canvas.appendChild(gridSquare);
     }
+
     const grids = document.querySelectorAll(".grid-square");
     grids.forEach(sq => sq.addEventListener("mouseover", changeColor));
 }
@@ -19,7 +23,6 @@ function changeColor(e) {
     e.target.style.backgroundColor = "red";
 }
 
-// Maintain a grid size of 480px no matter how many squares there are in the grid
 function getGridSquareSize(size) {
     return 480 / size + "px";
 }
